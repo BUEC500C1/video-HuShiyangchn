21ad55771deaf75a8e2de50f00b116c24aaebe73
hushiyangch <ancient_bamboo@163.com>
2020-02-22 23:20:31 -0500

    update

--- apimodule.py
+++ apimodule.py
@@ -1,9 +1,10 @@
 import tweepy
 import wget
+import os
+import datetime
+#import urllib2
 
-filename="output.txt"
-
-
+tweetsnum = 10
 consumer_key = "5BPSMCofACuMtRLPEWM5RGQoO"
 consumer_secret = "oKr8gArD2uwgOz4cnFzw7JqmmMyX51o3oMOkl5fhWSR591Gs1F"
 access_token ="1227718321874669568-tjbyvDD73So1buu8Wx8kPKSJBj8GJb"
@@ -11,12 +12,11 @@ access_token_secret="TceCoqwG0MtaaXDxjSSxPyCtGURSs4aMc4b5rMw2zTZJZ"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-
 api = tweepy.API(auth)
-
-tweets = api.user_timeline('LeoDiCaprio')
+tweets = api.user_timeline('nytimes',count=tweetsnum, tweet_mode='extended')
 
 media_files = set()
+
 for status in tweets:
     media = status.entities.get('media', [])
     if(len(media) > 0):
@@ -24,7 +24,58 @@ for status in tweets:
 
 for media_file in media_files:
     wget.download(media_file)
-with open(filename,'w') as fileobject:
-    for tweet in tweets:
-        fileobject.write(tweet.text)
-        fileobject.write('\n')
+
+direct = os.getcwd()
+tweetdir = os.path.join(direct,'tweets')
+
+contentlist = []
+#print(tweets)
+for tweet in tweets:
+    if (tweet.created_at.date() == datetime.date.today()):
+        contentlist.append(tweet.full_text)
+        #fileobject.write('\n')
+for items in contentlist:
+    print(items, end=' ')
+
+for i in range(len(contentlist)):
+    filename = 'tweet%s.txt' %(i)
+    filepath = os.path.join(tweetdir,filename)
+    with open(filepath,'w') as fileobject:
+        fileobject.write(contentlist[i])
+        #for tweet in tweets:
+'''
+tweets_images=[]
+tweets_videos=[]
+tweets_text=[]
+id_images=[]
+id_videos=[]
+public_tweets = api.user_timeline(screen_name="@yale",count=25,page=1,include_rts=False)
+all_items=[]
+[all_items.append(i) for i in public_tweets]
+
+for i in all_items:
+    try:
+        if i.entities['media'][0]['type']=='photo':
+            tweets_images.append({'url':i.entities['media'][0]['media_url'],'id':i.id})
+            id_images.append(i.id)
+    except:
+        pass
+
+for i in all_items:
+    try:
+        if i.id not in id_images:
+            if i.entities['urls'][0]['expanded_url']:
+                if 'youtube' in json.load(urllib2.urlopen('http://api.longurl.org/v2/expand?url='+i.entities%5B'urls'%5D%5B0%5D%5B'expanded_url'%5D+'&format=json&#8217;))['long-url']:
+                    tweets_videos.append({'url':i.entities['urls'][0]['expanded_url'],'id':i.id})
+                        id_videos.append(i.id)
+    except Exception as e:
+        pass
+
+for i in all_items:
+    if i.id not in id_images or i.id not in id_videos:
+    tweets_text.append({'text':i.text,'id':i.id})
+
+print tweets_text
+print tweets_images
+print tweets_videos
+'''
--- ffmpegmodule.py
+++ ffmpegmodule.py
@@ -1,10 +1,33 @@
 import os
 import subprocess
-with open('output.txt') as file_object:
-    content = file_object.read()
-    convert='ffmpeg -f lavfi -i color=c=blue:s=320x240:d=0.5 -vf \
-    "drawtext=fontfile=/path/to/font.ttf:fontsize=30: \
-    fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text=content" \
-    output.mp4'
-    #os.system(convert)
-    subprocess.call(convert,shell = True)
+#import text_to_image
+
+#text2image
+direct = os.getcwd()
+tweetdir = os.path.join(direct,'tweets')
+files = os.listdir(tweetdir)
+imagepath = os.path.join(direct,'tweetimages')
+'''
+num = 0
+for items in files:
+    print(items)
+for items in files:
+    with open(tweetdir + '/' +items) as file_object:
+        content = file_object.read()
+        convert='ffmpeg -f lavfi -i color=c=white:s=1920x1080:d=0.5 -vf \
+        "drawtext=fontfile=/path/to/font.ttf:fontsize=30: \
+        fontcolor=black:x=(w-text_w)/2:y=(h-text_h)/2:textfile=%s/%s" \
+        -frames:v 1 %s/image%03d.png' %(tweetdir, items, imagepath, num)
+        os.system(convert)
+        num = num + 1
+        with open('input.txt','a') as inputfile:
+            inputfile.write('%s/%s\n'%(tweetdir, items))
+            inputfile.write('duration 3\n')
+        
+'''
+
+    #subprocess.call(convert,shell = True)
+
+#image2video
+convertvideo = 'ffmpeg -loop 1 -f image2 -i '+ imagepath+'/image%03d.png -vcodec libx264 -r 10 -t 10 tweetdaily.mkv' 
+os.system(convertvideo)
